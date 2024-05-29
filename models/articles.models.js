b79d8d3b348a9289084ec5cd6ab6a698f4229426
Jamie Brawn <jameshenrybrawn@gmail.com>
2024-05-29 13:31:57 +0100

    adds GET /api/articles to endpoint with tests

--- models/articles.models.js
+++ models/articles.models.js
@@ -11,3 +11,17 @@ exports.selectArticleById = ( article_id ) => {
       return article;
     });
 };
+
+exports.selectArticles = () => {
+  return db
+  .query(
+    `SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id)::int AS comment_count
+    FROM articles
+    LEFT JOIN comments ON articles.article_id = comments.article_id
+    GROUP BY articles.article_id
+    ORDER BY articles.created_at DESC;`
+  )
+  .then((result) => {
+    return result.rows;
+  });
+};
