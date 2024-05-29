fa4e1e27f7439cbb7466daee17facfeeed7ffcd1
Jamie Brawn <jameshenrybrawn@gmail.com>
2024-05-29 11:46:07 +0100

    adds GET /api/articles/:article_id endpoint with status 200, 404 and 400 response tests

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const express = require('express') ;
 const app = express() ;
-const {
-  getTopics
-} = require( "./controllers/topics.controllers" ) ; 
+const { getTopics } = require( "./controllers/topics.controllers" ) ; 
+const { getArticleById } = require( "./controllers/articles.controllers" ) ;
 
 app.get( "/api/topics" , getTopics) ;
 
-
-
+app.get( "/api/articles/:article_id" , getArticleById ) ;
 
 
 
 // ERROR HANDLING
-
 
 // missing endpoint 404
 app.use( ( req , res , next ) => {
